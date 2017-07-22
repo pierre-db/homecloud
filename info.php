a1dd334506642954173dcd4e6bb6167cb80dc07e
Pierre <pierre.dalbianco@free.fr>
2017-07-22 14:42:29 +0700

    php/

---
+++ info.php
@@ -0,0 +1,26 @@
+<?php
+/*
+Copyright 2016 Pierre DAL BIANCO
+
+    This file is part of Home Cloud.
+
+    Home Cloud is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    Home Cloud is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with Home Cloud.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+    echo 'Home Cloud v1.0, 2016<br/>';
+    echo 'Visite the official website of the project <a rel="website" href="http://pierre.dalbianco.free.fr/homecloud" target="_blanck">here</a><br/>';
+    echo 'Home Cloud by Pierre DAL BIANCO is licensed under a <a rel="license" href="http://www.gnu.org/licenses/" target="_blanck">GNU General Public License</a>.';
+
+?>
+
